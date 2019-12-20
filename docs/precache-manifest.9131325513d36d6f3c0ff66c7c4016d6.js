13ecd53f50ae65151aa14dbdf9a57460405f3f29
Ana Arribas <arribas.ibd@gmail.com>
2019-12-20 17:11:09 +0100

    another commit

---
+++ docs/precache-manifest.9131325513d36d6f3c0ff66c7c4016d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bc25ccfce3c1073add03ac3d3200d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bfea568630e10cc9428",
+    "url": "./static/css/main.a7bcbe66.chunk.css"
+  },
+  {
+    "revision": "87cbc70570c8bbe92582",
+    "url": "./static/js/2.ff175af1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ff175af1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bfea568630e10cc9428",
+    "url": "./static/js/main.ce247a40.chunk.js"
+  },
+  {
+    "revision": "1698967487f20b08d06f",
+    "url": "./static/js/runtime-main.a5100ee7.js"
+  },
+  {
+    "revision": "aec4ed3af6feb66dd7c52eabc68f8fe6",
+    "url": "./static/media/llamas.aec4ed3a.jpeg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b42fc1a8b96278ca24d89b7a2cb62b36.js"
+  "./precache-manifest.9131325513d36d6f3c0ff66c7c4016d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
